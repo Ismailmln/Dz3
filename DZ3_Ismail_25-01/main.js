7388c5d9f547ce3f096c6e9ca2628ac454736330
Ismailmln <126384455+Ismailmln@users.noreply.github.com>
2023-04-15 14:15:17 +0600

    dz3

---
+++ DZ3_Ismail_25-01/main.js
@@ -0,0 +1,30 @@
+
+// function number (num1, num2) {
+//     console.log(Math.min(num1, num2))
+// }
+// number(22, 44)
+
+
+// function str() {
+//     const input = prompt('Введите цифру')
+//     console.log('Длинна строки'+ input.length)
+// }
+// str()
+
+
+// function calculator (num1, operator, num2) {
+//     if (operator === '+') {
+//      return num1 + num2
+//     }else if (operator === '-') {
+//         return num1 - num2
+//     }else if (operator === '*') {
+//         return num1 * num2
+//     } else if (operator === '/') {
+//         return num1 / num2
+//     }else {
+//         return 'Оператор не найден'
+//     }
+// }
+// console.log(calculator(5, '+', 5))
+// console.log(calculator(12345, '/', 5))
+// console.log(calculator(5, '*', 5))
